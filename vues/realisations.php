630a2353cfcdc1082df152e386a15f53a5319be1
GARCIA Y <33959681+FRGarciaY@users.noreply.github.com>
2018-01-05 12:12:49 +0100

    Update realisations.php

--- vues/realisations.php
+++ vues/realisations.php
@@ -7,7 +7,7 @@
         <div class="col-xs-12 col-sm-6 policeSpecial2 padding15">
             <br>
             <p>C'est le site que vous visitez en ce moment. Il a été réalisé en HTLM5, CSS3 (Bootstrap) et jQuery.</p>
-            <p>Le site se charge, puis des instruction en jquery permettent de changer la mise en page si javascript est activé sur le navigateur.</p>
+            <p>Le site se charge, puis des instructions en jquery permettent de changer la mise en page si javascript est activé sur le navigateur.</p>
             <p>Il est fonctionnel que javascript soit activé ou non. Seul change le confort de navigation.</p>
             <br>
             <br>
@@ -30,7 +30,7 @@
         <div class="col-xs-12 col-sm-6 policeSpecial2 padding15">
             <h2 class="text-center policeSpecial">La base de données</h2>
             <br>
-            <p>Après avoir étudié le cahier des charges, nous en avons déduit les besoins de l'application. Nous avons donc modélisé la base de données, puis sous Symfony 3, créée les entités (objets). Nous avons ensuite réalisé le mapping, c'est à dire les relations entre les objets, puis grace à <a href="https://fr.wikipedia.org/wiki/Doctrine_(ORM)" title="Definition de Doctrine selon Wikipedia">Doctrine</a>, ORM par defaut de Symfony, nous avons généré la base de données MySQL correspondante.</p>
+            <p>Après avoir étudié le cahier des charges, j'en ai déduit les besoins de l'application. J'ai donc modélisé la base de données, puis sous Symfony 3, créée les entités (objets). J'ai ensuite réalisé le mapping, c'est à dire les relations entre les objets, puis grace à <a href="https://fr.wikipedia.org/wiki/Doctrine_(ORM)" title="Definition de Doctrine selon Wikipedia">Doctrine</a>, ORM par defaut de Symfony, j'ai généré la base de données MySQL correspondante.</p>
             <br>    
             <a href="telechargement/mcd.jpg" target="_blank"><span><img id="jpg" src="images/jpg.png" alt=""/></span>Télécharger le MCD</a>
             <br>
@@ -48,7 +48,7 @@
             <h2 class="text-center policeSpecial">L'application</h2>
             <br>
             <p>Une fois la base de données prête, il reste à développer l'application qui permettra de l'interroger ou la mettre à jour. Pour cela Symfony permet de créer des formulaires à l'intérieur du controleur, Doctrine fait le lien avec la base de données, puis ensuite à l'intérieur des vues, <a href="https://fr.wikipedia.org/wiki/Twig" title="Definition de Twig selon Wikipedia">Twig</a> simplifie le code en évitant d'insérer des variabes php.</p>
-            <p>Nous avons utiliser Tortoise SVN pour gérer les différentes versions de notre code</p>
+            <p>Afin de simplifier la gestion des versions de notre code, nous avons utilisé Tortoise SVN.</p>
             <br>
             <a href="telechargement/Documentation.pdf" target="_blank"><span><img id="pdf" src="images/pdf_icon.png" alt=""/></span>Télécharger la documentation</a>
             <br>
@@ -73,12 +73,12 @@
                     L'ajout, la modification et la suppression d'un médicament offert dans un rapport de visite
                 </li>
             </ul>
-            <p>Les Tables MEDICAMENT, FAMILLE, OFFRIR, RAPPORT-VISITE, PRATICIEN sont concernés</p>
+            <p>Les Tables MEDICAMENT, FAMILLE, OFFRIR, RAPPORT-VISITE, PRATICIEN sont concernées</p>
         </div>
         <div class="col-xs-12 col-sm-6 policeSpecial2 padding15">
             <h2 class="text-center policeSpecial">L'application Web</h2>
             <br>
-            <p>J'ai choisi de dévelloper cette application Web en PHP avec le framwork Symfony, ainsi qu'une base de données MySql<br>
+            <p>J'ai choisi de développer cette application Web en PHP avec le framwork Symfony, ainsi qu'une base de données MySql<br>
             <br>
             <a href="https://github.com/FRGarciaY/GSB_WEB" target="_blank"><span><img id="jpg" src="images/github.gif" alt=""/></span>Code source</a>
             
@@ -98,7 +98,7 @@
         <div class="col-xs-12 col-sm-6 policeSpecial2 padding15">
             <h2 class="text-center policeSpecial">L'application Desktop</h2>
             <br>
-            <p>J'ai choisi de dévelloper ma version Desktop de GSB dans le langage C# en Windows Forms avec le framwork .net et linq</p>
+            <p>J'ai choisi de développer ma version Desktop de GSB dans le langage C# en Windows Forms avec le framwork .net et linq</p>
             <br>
             <a href="https://github.com/FRGarciaY/GSB_DESKTOP" target="_blank"><span><img id="jpg" src="images/github.gif" alt=""/></span>Code source</a>
             <br>
@@ -124,7 +124,7 @@
             <h2 class="text-center policeSpecial">Calculatrice</h2>
             <br>
             <p>Ce projet, réalisé en langage C#, est une application Windows Forms.</p>
-            <p>Les fonctionnalitées sont celle d'une calculatrice standard.</p>
+            <p>Les fonctionnalités sont celle d'une calculatrice standard.</p>
             <p>L'application est composée de deux formulaires : la calculatrice et une fenêtre "a propos".</p>
             <br>
             <br>
