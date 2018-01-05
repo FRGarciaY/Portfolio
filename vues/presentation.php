3fa743b81eb439e86c9952eabe731d19dbc1bc24
GARCIA Y <33959681+FRGarciaY@users.noreply.github.com>
2018-01-05 12:12:29 +0100

    Update presentation.php

--- vues/presentation.php
+++ vues/presentation.php
@@ -23,7 +23,7 @@
             <h2 class="policeSpecial">Qu'est ce que le BTS SIO ?</h2>
             <p>Le BTS Services Informatiques aux Organisations est un diplôme reconnu par l'état de niveau Bac+2. Il remplace depuis septembre 2011
                 l'ancien BTS Informatique de Gestion (BTS IG). Il associe aux enseignements théoriques et professionnels (en contrôle continu et final) 
-                un stage en entreprise de 10 semaines. Il existe en deux spécialités bien distinctes&nbsp: le BTS SIO SISR et le BTS SIO SLAM.
+                un stage en entreprise de 10 semaines. Il existe en deux spécialités bien distinctes&nbsp: le BTS SIO SISR (Solutions d'Infrastructure, Systèmes et Réseaux), et le BTS SIO SLAM (Solutions Logicielles et Applications Métiers).
             </p>
         </div>
 
@@ -34,7 +34,7 @@
         </div>
         <div class="col-xs-12 col-sm-9 vertical-align">
             <h2 class="policeSpecial">Le BTS SIO option SLAM</h2>
-            <p>L'option Solutions logicielles et applications métiers forme des spécialistes des logiciels. L'objectif de ce BTS est de former 
+            <p>L'option Solutions Logicielles et Applications Métiers forme des spécialistes des logiciels. L'objectif de ce BTS est de former 
                 à développer, à adapter et à maintenir des solutions applicatives.
             </p>
         </div>
